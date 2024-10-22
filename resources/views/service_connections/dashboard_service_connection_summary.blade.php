5f78b894e3a6182cc045794bb68b91bfaa199362
Julio Lopez <julzasdf123456@gmail.com>
2024-10-22 11:30:00 +0800

    modified tickets search

--- resources/views/service_connections/dashboard_service_connection_summary.blade.php
+++ resources/views/service_connections/dashboard_service_connection_summary.blade.php
@@ -208,7 +208,7 @@
                         datasets: [
                         {
                             label: 'Received',
-                            backgroundColor: '#c4bd5d',
+                            backgroundColor: 'rgba(196, 189, 93, .8)',
                             borderColor: '#c4bd5d',
                             pointRadius: true,
                             pointColor: '#3b8bba',
