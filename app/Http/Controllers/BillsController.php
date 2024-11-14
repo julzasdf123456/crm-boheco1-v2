dc7d347643882536b551f2a85644b71858ce694b
Julio Lopez <julzasdf123456@gmail.com>
2024-11-14 09:07:06 +0800

    auto email modfications

--- app/Http/Controllers/BillsController.php
+++ app/Http/Controllers/BillsController.php
@@ -452,7 +452,7 @@ class BillsController extends AppBaseController
         $bill = DB::connection("sqlsrvbilling")
             ->table("Bills")
             ->leftJoin("AccountMaster", "AccountMaster.AccountNumber", "=", "Bills.AccountNumber")
-            ->whereRaw("AccountMaster.Email IS NOT NULL AND Bills.EmailSent IS NULL AND Bills.ServicePeriodEnd > '2024-09-01'")
+            ->whereRaw("AccountMaster.Email IS NOT NULL AND Bills.EmailSent IS NULL AND Bills.ServicePeriodEnd > '2024-10-01'")
             // ->whereRaw("AccountMaster.Email LIKE '%julzasdf%' AND Bills.EmailSent IS NULL")
             ->select("Bills.*")
             ->orderByDesc('ServicePeriodEnd')
